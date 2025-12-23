16d0b23d0dba127d0beede2b2d000a634cba4886
Tanner Ellen <tanner@seedcode.com>
2025-12-22 22:49:01 -0800

    chore: remove compress file step from build

--- build.sh
+++ build.sh
@@ -2,7 +2,4 @@
 
 mkdir -p ./bin
 
-bun build --compile --minify --sourcemap --bytecode --target=bun-linux-x64 ./index.js --outfile bin/tufi
-cd bin
-tar -czf tufi-linux-amd64.tar.gz tufi
-
+bun build --compile --minify --bytecode --target=bun-linux-x64 ./index.js --outfile bin/tufi
